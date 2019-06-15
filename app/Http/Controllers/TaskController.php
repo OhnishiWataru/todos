0377e765f82787c68289f850ad8abf5b8b19f195
OhnishiWataru <ohnishi.wataru.business@gmail.com>
2019-06-15 19:26:17 +0900

    folder

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,26 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
-use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index(int $id)
-    {
-        // すべてのフォルダを取得する
-        $folders = Folder::all();
+  public function index(int $id)
+  {
+    // すべてのフォルダを取得する
+    $folders = Folder::all();
 
-        // 選ばれたフォルダを取得する
-        $current_folder = Folder::find($id);
+    // 選ばれたフォルダを取得する
+    $current_folder = Folder::find($id);
 
-        // 選ばれたフォルダに紐づくタスクを取得する
-        $tasks = Task::where('folder_id', $current_folder->id)->get();
+    // 選ばれたフォルダに紐づくタスクを取得する
+    $tasks = $current_folder->tasks()->get();
 
-        return view('tasks/index', [
-            'folders' => $folders,
-            'current_folder_id' => $current_folder->id,
-            'tasks' => $tasks,
-        ]);
-    }
+    return view('tasks/index', [
+      'folders' => $folders,
+      'current_folder_id' => $current_folder->id,
+      'tasks' => $tasks,
+    ]);
+  }
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -20,7 +20,7 @@
           <nav class="panel panel-default">
             <div class="panel-heading">フォルダ</div>
             <div class="panel-body">
-              <a href="#" class="btn btn-default btn-block">
+              <a href="{{ route('folders.create') }}" class="btn btn-default btn-block">
                 フォルダを追加する
               </a>
             </div>
@@ -58,9 +58,9 @@
                   <tr>
                     <td>{{ $task->title }}</td>
                     <td>
-                      <span class="label">{{ $task->status_label }}</span>
+                      <span class="label {{ $task->status_class }}">{{ $task->status_label }}</span>
                     </td>
-                    <td>{{ $task->due_date }}</td>
+                    <td>{{ $task->formatted_due_date }}</td>
                     <td><a href="#">編集</a></td>
                   </tr>
                 @endforeach
