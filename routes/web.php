688103fc3a8a861c08c0ca9415a91db3c0d07e2b
OhnishiWataru <ohnishi.wataru.business@gmail.com>
2019-06-19 20:41:13 +0900

    register

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::post('/folders/create', 'FolderController@create');
 
 Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
