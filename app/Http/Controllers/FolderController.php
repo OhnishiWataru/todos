cf9251edced36f191d147a3c307462080ea62d46
OhnishiWataru <ohnishi.wataru.business@gmail.com>
2019-06-23 15:20:50 +0900

    use plus

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -4,32 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Folder;
 use App\Http\Requests\CreateFolder;
-use Illuminate\Support\Facades\Auth;
-
-//クラスのインポート
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FolderController extends Controller
 {
     public function showCreateForm()
     {
-      return view('folders/create');
+        return view('folders/create');
     }
-
-    //引数にインポートしたRequestクラスを受け入れる
     public function create(CreateFolder $request)
     {
-      //フォルダモデルのインスタンスを作成する
-      $folder = new Folder();
+        // フォルダモデルのインスタンスを作成する
+        $folder = new Folder();
 
-      //タイトルに入力値を代入する
-      $folder->title = $request->title;
+        // タイトルに入力値を代入する
+        $folder->title = $request->title;
 
-      //インスタンスの状態をデータベースに書き込む
-      Auth::user()->folders()->save($folder);
+        // インスタンスの状態をデータベースに書き込む
+        Auth::user()->folders()->save($folder);
 
-      return redirect()->route('tasks.index', [
-          'id' => $folder->id,
-      ]);
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
     }
 }
